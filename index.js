719017248d7fd69cb3d4ada1dc6cc5d672f47d37
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-25 04:26:38 +0600

    added cors middleware

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-
+const cors = require('cors')
 const port = process.env.PORT || 5000
 const app = express()
+
+app.use(express.json())
+app.use(cors({
+    origin: ['https://todo-list-frontend-eta.vercel.app','http://localhost:5173']
+}))
 
 app.get('/',(req,res)=>{
     res.send('TODO LIST SERVER')
