30e6a7a1e523b58ed9c7d49238d7e6e1f11ce3f6
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-28 00:17:35 +0600

    added mongodb connection string

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -8,9 +9,41 @@ app.use(cors({
     origin: ['https://todo-list-frontend-eta.vercel.app','http://localhost:5173']
 }))
 
-app.get('/',(req,res)=>{
-    res.send('TODO LIST SERVER')
-})
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@database1.g36ghl5.mongodb.net/?retryWrites=true&w=majority&appName=database1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const userCollection = 
+
+    app.get('/')
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
