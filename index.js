1af11d1e47ffb3af5da879cd5e324e34663923c2
Shahriar-Samir <shabusiness035@gmail.com>
2024-07-01 17:07:15 +0600

    added many functionalities

--- index.js
+++ index.js
@@ -36,7 +36,10 @@ async function run() {
 
     const isPastDue = (dueDate, dueTime) => {
       const now = new Date();
-      const newDueTime = dueTime===''? '00:00' : dueTime 
+      if(dueDate ==='' && dueTime===''){
+        return false
+      }
+      const newDueTime = dueTime===''? '23:59' : dueTime 
       const dueDateTime = new Date(dueDate);
       const [time, modifier] = newDueTime.split(' ');
       let [hours, minutes] = time.split(':').map(Number);
@@ -45,6 +48,7 @@ async function run() {
       dueDateTime.setHours(hours, minutes, 0, 0);
       return now > dueDateTime;
     };
+    
     const isPastReminder = (dueDate, reminderTime) => {
       const now = new Date();
       if(reminderTime === ""){
@@ -105,23 +109,25 @@ async function run() {
         const notification = {
             title: "You've missed the task to finish on time",
             uid: task?.uid,
-            description: `The due date and time for the task was ${task?.dueDate} ${task?.dueTime}. But you are late to finish the work on time`
+            description: `The due date and time for the task was ${task?.dueDate} ${task?.dueTime}. But you are late to finish the work on time`,
+            readStatus: false,  
+            createdAt: Date.now()
         }
         await notificationCollection.insertOne(notification)
-        console.log('send a  notification')
         return taskCollection.updateOne(
           { _id: task._id },
-          { $set: { status: 'unfinished' } }
+          { $set: { status: 'unfinished' } },
         );
       }
       if (isPastReminder(task.dueDate, task.reminderTime) && task.status === 'upcoming' && task.reminderTime !== '' && !task?.reminderStatus) {
         const notification = {
             title:`⚠️Reminder: You have ${subtractTimes(task.dueTime,task.reminderTime)}`,
             uid: task?.uid,
-            description: `You have ${subtractTimes(task.dueTime,task.reminderTime)}`
+            description: `You have ${subtractTimes(task.dueTime,task.reminderTime)}`,
+            readStatus: false,
+            createdAt: Date.now()
         }
         await notificationCollection.insertOne(notification)
-        console.log('send a reminder notification')
         return taskCollection.updateOne(
           { _id: task._id },
           { $set: { status: 'upcoming', reminderStatus: true } }
@@ -150,21 +156,70 @@ async function run() {
 
     app.get('/notifications/:uid',async(req,res)=>{
         const {uid} = req.params
-        const getData = await notificationCollection.find({uid}).toArray()
+        const getData = await notificationCollection.find({uid}).sort({createdAt:-1}).toArray()
         res.send(getData)
     })
+  
 
     app.get('/userTasksAll/:uid',async(req,res)=>{
         const {uid} = req.params
+        const getAllTasks = await taskCollection.find({uid, status: {$in:['upcoming','unfinished']}}).sort({createdAt:-1}).toArray()
+        res.send(getAllTasks)
+    })
+    app.get('/userTasksAllEvents/:uid',async(req,res)=>{
+        const {uid} = req.params
         const getAllTasks = await taskCollection.find({uid}).sort({createdAt:-1}).toArray()
         res.send(getAllTasks)
+    })
+    app.get('/userTasksAllAmounts/:uid',async(req,res)=>{
+        const {uid} = req.params
+        const getFinishedTasks = await taskCollection.find({uid, status:'finished'}).toArray()
+        const getUnfinishedTasks = await taskCollection.find({uid, status: 'unfinished'}).toArray()
+        const getUpcomingTasks = await taskCollection.find({uid, status:'upcoming'}).toArray()
+        const finishedTasksLength =  getFinishedTasks.length
+        const unfinishedTasksLength =  getUnfinishedTasks.length
+        const upcomingTasksLength =  getUpcomingTasks.length
+        res.send({finishedTasksLength,unfinishedTasksLength,upcomingTasksLength})
+    })
+
+    app.patch('/markAsRead/:id',async(req,res)=>{
+        const {id} = req.params
+        const options = {upsert:true}
+        const updatedData = {
+          $set:{
+            readStatus: true
+          }
+        }
+        const markAsRead = await notificationCollection.updateOne({_id: new ObjectId(id)}, updatedData, options)
+        res.send(markAsRead)
+    })
+
+    
+    app.get('/userTasksAmounts/:uid',async(req,res)=>{
+        const {uid} = req.params
+        const date = new Date()
+        const currentDate = date.toDateString()
+        const getTodayTasks = await taskCollection.find({uid, dueDate:currentDate, status: {$in:['upcoming','unfinished']}}).toArray()
+        const getAllTasks = await taskCollection.find({uid, status: {$in:['upcoming','unfinished']}}).toArray()
+        const getNotifications = await notificationCollection.find({uid, readStatus: false}).sort({createdAt:-1}).toArray()
+        const notificationsLength = getNotifications.length
+        const allTasksLength = getAllTasks.length
+        const todayTasksLength = getTodayTasks.length
+        res.send({allTasksLength, todayTasksLength, notificationsLength})
+    })
+
+
+    app.delete('/deleteUserTask/:id',async(req,res)=>{
+        const {id} = req.params
+        const deleteTask = await taskCollection.deleteOne({_id: new ObjectId(id)})
+        res.send(deleteTask)
     })
 
     app.get('/userTasksToday/:uid',async(req,res)=>{
         const date = new Date()
         const currentDate = date.toDateString()
         const {uid} = req.params
-        const getAllTasks = await taskCollection.find({uid, dueDate:currentDate}).sort({createdAt:-1}).toArray()
+        const getAllTasks = await taskCollection.find({uid, dueDate:currentDate, status:{$in:['upcoming','unfinished']}}).sort({createdAt:-1}).toArray()
         res.send(getAllTasks)
     })
 
@@ -199,6 +254,20 @@ async function run() {
         const updateData = await taskCollection.updateOne(filter,updatedData,options)
         res.send(updateData)
     })
+
+    app.patch('/checkTask/:id', async(req,res)=>{
+          const {id} = req.params
+          const filter = new ObjectId(id)
+          const options = {upsert:true}
+          const data = {
+            $set:{
+              status: 'finished'
+            }
+          }
+          const checkTask = await taskCollection.updateOne({_id:filter},data,options)
+          res.send(checkTask)
+        })
+
 
     app.patch('/updateUser', (req,res)=>{
           const userData = req.body
